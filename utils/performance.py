1b65bac24e668acf916a1de52b0ea5ae478bdd0d
Marlon Marin <marlonmariin97@outlook.com>
2024-06-24 10:18:20 -0500

    (build) 4-final_project and utils/performance.py, function related to performance_plot() were moved to utils/performance.py (build) Minor changes related to markdown in 2-cars_dataset.ipynb

--- utils/performance.py
+++ utils/performance.py
@@ -6,6 +6,13 @@ import category_encoders as ce
 import matplotlib.pyplot as plt
 import pandas as pd
 
+def object_type(X_set):
+    dicty = X_set.dtypes.to_dict()
+    cols_to_encode = []
+    for key, values in dicty.items():
+        if values == "object":
+            cols_to_encode.append(key)
+    return cols_to_encode
 
 def performance_dataframe(n_estimators, X, y) -> pd.DataFrame:   
     perform_dict = {"test" : [], "train": []}
@@ -14,10 +21,12 @@ def performance_dataframe(n_estimators, X, y) -> pd.DataFrame:
         # Spliting data, 30% for training and 70% for testing.
         X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.3)
         
-        # Encoding data
-        encoder = ce.OrdinalEncoder(cols=['buying_price', 'maint_cost', 'n_doors', 'n_person', 'lug_boot', 'safety'])
-        X_train = encoder.fit_transform(X_train)
-        X_test  = encoder.transform(X_test)
+        # Encoding data from only categorical type within X columns.
+        cols_to_encode = object_type(X)
+        if len(cols_to_encode) > 0:
+            encoder = ce.OrdinalEncoder(cols=cols_to_encode)
+            X_train = encoder.fit_transform(X_train)
+            X_test  = encoder.transform(X_test)
         
         # Creating Random Forest model
         random_forest = RandomForestClassifier(n_estimators=i)
@@ -41,17 +50,26 @@ def performance_dataframe(n_estimators, X, y) -> pd.DataFrame:
 
     return perform_data
 
-def performance_plot(n_estimators, X, y) -> plt.plot:   
+def performance_plot(n_estimators, X, y) -> plt.plot:
+    """
+        performance_plot function takes input X, y data from a clean dataset and calculate the performance of RandomForestClassifier given a number of estimators.
+        Arguments:
+            n_estimators: the number of estimators to be included in Random Forest Classifier method
+            X: X columns from cleaned dataset, if categorical variables are present, these will be encoded using function object_type()
+            y: y column from cleaned dataset, always a numerical variable.
+    """   
     perform_dict = {"test" : [], "train": []}
     
     for i in range(1, n_estimators+1):
         # Spliting data, 30% for training and 70% for testing.
         X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, test_size=0.3)
         
-        # Encoding data
-        encoder = ce.OrdinalEncoder(cols=['buying_price', 'maint_cost', 'n_doors', 'n_person', 'lug_boot', 'safety'])
-        X_train = encoder.fit_transform(X_train)
-        X_test  = encoder.transform(X_test)
+        # Encoding data from only categorical type within X columns.
+        cols_to_encode = object_type(X)
+        if len(cols_to_encode) > 0:
+            encoder = ce.OrdinalEncoder(cols=cols_to_encode)
+            X_train = encoder.fit_transform(X_train)
+            X_test  = encoder.transform(X_test)
         
         # Creating Random Forest model
         random_forest = RandomForestClassifier(n_estimators=i)
@@ -82,7 +100,8 @@ def performance_plot(n_estimators, X, y) -> plt.plot:
             )
         )
     )
-
+    
+    # Plot adjustment.
     plt.grid()
     plt.xlabel("Number of iterations")
     plt.ylabel("Performance")
